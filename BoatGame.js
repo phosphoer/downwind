80f9cbe0483feac522fb963f2ec7f2d2e49df692
David Evans <david0evans@gmail.com>
2013-07-06 00:25:31 -0700

    Ocean scrolls now

--- BoatGame.js
+++ BoatGame.js
@@ -2,20 +2,20 @@
 
 function main()
 {
-    var space = TANK.createSpace("InputManager", "Graphics", "Stats");
-    TANK.addSpace(space, "Game");
+  var space = TANK.createSpace("InputManager", "Graphics", "Stats");
+  TANK.addSpace(space, "Game");
 
-    var boat = TANK.createEntity("Transform", "Box", "Skybox", "Model", "Controller");
-    boat.Box.diffuse.setRGB(0.9, 0.9, 0.9);
-    boat.Skybox.skyColor.setHex(0xCCFFFF);
-    boat.Skybox.baseColor.setHex(0x2F4F8F);
-    boat.Skybox.scale.set(5000, 5000, 5000);
-    boat.Model.model = Boat;
-    space.addEntity(boat, "Boat");
+  var boat = TANK.createEntity("Transform", "Box", "Skybox", "Model", "Controller");
+  boat.Box.diffuse.setRGB(0.9, 0.9, 0.9);
+  boat.Skybox.skyColor.setHex(0xCCFFFF);
+  boat.Skybox.baseColor.setHex(0x2F4F8F);
+  boat.Skybox.scale.set(5000, 5000, 5000);
+  boat.Model.model = Boat;
+  space.addEntity(boat, "Boat");
 
-    var ocean = TANK.createEntity("Ocean");
-    space.addEntity(ocean, "Ocean");
+  var ocean = TANK.createEntity("Ocean");
+  space.addEntity(ocean, "Ocean");
 
-    TANK.start();
+  TANK.start();
 
 }
--- src/components/Ocean.js
+++ src/components/Ocean.js
@@ -8,10 +8,11 @@
 
   .construct(function ()
   {
-    this.node = new THREE.Object3D();;
+    this.node = new THREE.Object3D();
     this.cubeScale = 10;
-    this.patchSize = 10;
+    this.patchSize = 5;
     this.numPatches = 3;
+    this.totalSize = this.numPatches * this.patchSize * this.cubeScale;
     this.patches = {};
   })
 
@@ -39,9 +40,11 @@
           patch.cubes[i] = {};
           for (var j = 0; j < this.patchSize; ++j)
           {
+            var color = new THREE.Color();
+            color.setRGB(x / (this.numPatches - 1), 0, z / (this.numPatches - 1));
             patch.cubes[i][j] = new global.THREE.Mesh(g.unitCube, new global.THREE.MeshBasicMaterial(
             {
-              color: new THREE.Color(0x1133ee),
+              color: color, //new THREE.Color(0x1133ee),
               wireframe: true
             }));
             patch.cubes[i][j].position = new THREE.Vector3(i * this.cubeScale, 0, j * this.cubeScale);
@@ -52,6 +55,70 @@
       }
     }
 
+    this.scrollLeft = function ()
+    {
+      for (var i = 0; i < this.numPatches; ++i)
+      {
+        var fromPatch = this.patches[this.numPatches - 1][i];
+
+        for (var j = this.numPatches - 1; j > 0; --j)
+        {
+          this.patches[j][i] = this.patches[j - 1][i];
+        }
+
+        this.patches[0][i] = fromPatch;
+        fromPatch.node.position.x -= this.numPatches * this.patchSize * this.cubeScale;
+      }
+    }
+
+    this.scrollRight = function ()
+    {
+      for (var i = 0; i < this.numPatches; ++i)
+      {
+        var fromPatch = this.patches[0][i];
+
+        for (var j = 0; j < this.numPatches - 1; ++j)
+        {
+          this.patches[j][i] = this.patches[j + 1][i];
+        }
+
+        this.patches[this.numPatches - 1][i] = fromPatch;
+        fromPatch.node.position.x += this.numPatches * this.patchSize * this.cubeScale;
+      }
+    }
+
+    this.scrollUp = function ()
+    {
+      for (var i = 0; i < this.numPatches; ++i)
+      {
+        var fromPatch = this.patches[i][this.numPatches - 1];
+
+        for (var j = this.numPatches - 1; j > 0; --j)
+        {
+          this.patches[i][j] = this.patches[i][j - 1];
+        }
+
+        this.patches[i][0] = fromPatch;
+        fromPatch.node.position.z -= this.numPatches * this.patchSize * this.cubeScale;
+      }
+    }
+
+    this.scrollDown = function ()
+    {
+      for (var i = 0; i < this.numPatches; ++i)
+      {
+        var fromPatch = this.patches[i][this.numPatches - 1];
+
+        for (var j = this.numPatches - 1; j > 0; --j)
+        {
+          this.patches[i][j] = this.patches[i][j - 1];
+        }
+
+        this.patches[i][0] = fromPatch;
+        fromPatch.node.position.z -= this.numPatches * this.patchSize * this.cubeScale;
+      }
+    }
+
 
     this.addEventListener("OnEnterFrame", OnEnterFrame);
   });
@@ -59,7 +126,20 @@
   function OnEnterFrame(dt)
   {
     var camera = TANK.Game.getEntity("Boat");
+    var camPos = camera.Transform.position;
     var myPos = this.parent.Transform.position;
+    // camPos.x += dt * 2;
+    // camPos.z -= dt * 2;
+
+    myPos.x = -this.numPatches * this.patchSize * this.cubeScale * 0.5;
+    myPos.z = -this.numPatches * this.patchSize * this.cubeScale * 0.5;
+
+    if (camPos.x - myPos.x < this.patches[this.numPatches - 1][0].node.position.x - this.patchSize * this.cubeScale)
+      this.scrollLeft();
+    if (camPos.x - myPos.x > this.patches[0][0].node.position.x + 2 * this.patchSize * this.cubeScale)
+      this.scrollRight();
+    if (camPos.z - myPos.z < this.patches[0][this.numPatches - 1].node.position.z - this.patchSize * this.cubeScale)
+      this.scrollUp();
   }
 
 }(this, this.TANK = this.TANK ||
