9d77133307dc5894e5e88456ff0a0919e20f1538
kkoenig <devnull@localhost>
2013-07-05 20:53:04 -0700

    Added a skybox.

--- BoatGame.js
+++ BoatGame.js
@@ -6,10 +6,12 @@ function main()
     TANK.addSpace(space, "Game");
 
     var boat = TANK.createEntity();
-    boat.addComponents("Transform", "Box");
+    boat.addComponents("Transform", "Box", "Skybox");
     boat.Box.diffuse.setRGB(0.9, 0.9, 0.9);
     boat.Transform.scale.set(10, 10, 20);
-    //boat.Transform.rotation.set(Math.random(), Math.random(), Math.random());
+    boat.Skybox.skyColor.setHex(0xCCFFFF);
+    boat.Skybox.baseColor.setHex(0x2F4F8F);
+    boat.Skybox.scale.set(2000, 2000, 2000);
     space.addEntity(boat, "Boat");
 
     TANK.start();
