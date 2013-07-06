91ba6a7bb6b65e39b75bee7e760403c059c0e36d
kkoenig <devnull@localhost>
2013-07-05 21:39:07 -0700

    Added boat skeleton.

--- BoatGame.js
+++ BoatGame.js
@@ -2,20 +2,21 @@
 
 function main()
 {
-    var space = TANK.createSpace("InputManager", "Graphics", "Stats");
-    TANK.addSpace(space, "Game");
+  var space = TANK.createSpace("InputManager", "Graphics", "Stats");
+  TANK.addSpace(space, "Game");
 
-    var boat = TANK.createEntity("Transform", "Box", "Skybox", "Model", "Controller");
-    boat.Box.diffuse.setRGB(0.9, 0.9, 0.9);
-    boat.Skybox.skyColor.setHex(0xCCFFFF);
-    boat.Skybox.baseColor.setHex(0x2F4F8F);
-    boat.Skybox.scale.set(5000, 5000, 5000);
-    boat.Model.model = Boat;
-    space.addEntity(boat, "Boat");
+  var boat = TANK.createEntity("Transform", "Box", "Skybox", "Model", "Boat", "Controller");
+  boat.Box.diffuse.setRGB(0.9, 0.9, 0.9);
+  boat.Skybox.skyColor.setHex(0xCCFFFF);
+  boat.Skybox.baseColor.setHex(0x2F4F8F);
+  boat.Skybox.scale.set(5000, 5000, 5000);
+  boat.Model.model = Boat;
 
-    var ocean = TANK.createEntity("Ocean");
-    space.addEntity(ocean, "Ocean");
+  space.addEntity(boat, "Boat");
 
-    TANK.start();
+  var ocean = TANK.createEntity("Ocean");
+  space.addEntity(ocean, "Ocean");
+
+  TANK.start();
 
 }
