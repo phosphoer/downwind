9d4834e117129a1f46139c88d66062f2f777e4ab
kkoenig <devnull@localhost>
2013-07-05 22:10:02 -0700

    Added forward movement.

--- src/components/Boat.js
+++ src/components/Boat.js
@@ -7,13 +7,25 @@
 	.construct(function ()
 	{
 		this.turn = 0;
+		this.throttle = false;
+		this.speed = .2;
 	})
 
 	.initialize(function ()
 	{
 		this.addEventListener("OnEnterFrame", function (dt)
 		{
-			this.parent.Transform.rotation.y += this.turn * dt;
+			var t = this.parent.Transform;
+			t.rotation.y += this.turn * dt;
+
+			var facing = new global.THREE.Vector3(0, 0, 1);
+			var q = new global.THREE.Quaternion();
+			q.setFromEuler(t.rotation);
+			q.multiplyVector3(facing);
+			facing.normalize();
+			facing.multiplyScalar(this.speed);
+
+			t.position.addVectors(t.position, facing);
 		});
 	});
 
