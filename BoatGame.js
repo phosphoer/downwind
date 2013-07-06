4062ccde9c9fe639d724a5a6136102cbef4cc2d2
kkoenig <devnull@localhost>
2013-07-05 23:18:10 -0700

    Added awesome ship model.

--- BoatGame.js
+++ BoatGame.js
@@ -5,12 +5,12 @@ function main()
   var space = TANK.createSpace("InputManager", "Graphics", "Stats");
   TANK.addSpace(space, "Game");
 
-  var boat = TANK.createEntity("Transform", "Box", "Skybox", "Model", "Boat", "Controller");
-  boat.Box.diffuse.setRGB(0.9, 0.9, 0.9);
+  var boat = TANK.createEntity("Transform", "Skybox", "Model", "Boat", "Controller");
+
   boat.Skybox.skyColor.setHex(0xCCFFFF);
   boat.Skybox.baseColor.setHex(0x2F4F8F);
   boat.Skybox.scale.set(5000, 5000, 5000);
-  boat.Model.model = Boat;
+  boat.Model.model = PirateShip;
 
   boat.Boat.forwardSpeed = 8.2;
   boat.Boat.backwardSpeed = 4.4;
