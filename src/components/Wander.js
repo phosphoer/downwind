d40aad391a3b2594ec7abd065d2017d07ba7724c
kkoenig <devnull@localhost>
2013-07-06 00:56:29 -0700

    Added wandering and bounding boxes.

---
+++ src/components/Wander.js
@@ -0,0 +1,21 @@
+(function (global, TANK)
+{
+	"use strict";
+
+	TANK.registerComponent("Wander")
+
+	.requires("Boat")
+
+	.initialize(function ()
+	{
+		var turn = Math.random() > 0.5 ? 1 : -1;
+
+		this.parent.Boat.turn = turn;
+		this.parent.Boat.throttle = 1;
+		this.addEventListener("OnEnterFrame", function (dt) {
+
+		});
+	});
+
+}(this, this.TANK = this.TANK ||
+{}));
