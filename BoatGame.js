b26a964848ee846ccd08d86139ea090814f5f2d4
David Evans <david0evans@gmail.com>
2013-07-05 21:12:43 -0700

    Add ocean

--- BoatGame.js
+++ BoatGame.js
@@ -2,16 +2,19 @@
 
 function main()
 {
-    var space = TANK.createSpace("InputManager", "Graphics");
-    TANK.addSpace(space, "Game");
+  var space = TANK.createSpace("InputManager", "Graphics");
+  TANK.addSpace(space, "Game");
 
-    var boat = TANK.createEntity();
-    boat.addComponents("Transform", "Box");
-    boat.Box.diffuse.setRGB(0.9, 0.9, 0.9);
-    boat.Transform.scale.set(10, 10, 20);
-    //boat.Transform.rotation.set(Math.random(), Math.random(), Math.random());
-    space.addEntity(boat, "Boat");
+  var boat = TANK.createEntity();
+  boat.addComponents("Transform", "Box");
+  boat.Box.diffuse.setRGB(0.9, 0.9, 0.9);
+  boat.Transform.scale.set(10, 10, 20);
+  //boat.Transform.rotation.set(Math.random(), Math.random(), Math.random());
+  space.addEntity(boat, "Boat");
 
-    TANK.start();
+  var ocean = TANK.createEntity("Ocean");
+  space.addEntity(ocean, "Ocean");
+
+  TANK.start();
 
 }
