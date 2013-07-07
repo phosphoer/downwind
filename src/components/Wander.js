57330566161910a6bd2d2ab6b29bdf47b4c23c80
David Evans <david0evans@gmail.com>
2013-07-06 20:43:56 -0700

    Fix boat floating

--- src/components/Wander.js
+++ src/components/Wander.js
@@ -1,21 +1,21 @@
 (function (global, TANK)
 {
-	"use strict";
+  "use strict";
 
-	TANK.registerComponent("Wander")
+  TANK.registerComponent("Wander")
 
-	.requires("Boat")
+  .requires("Boat")
 
-	.initialize(function ()
-	{
-		var turn = Math.random() > 0.5 ? 1 : -1;
+  .initialize(function ()
+  {
+    var turn = Math.random() > 0.5 ? 1 : -1;
 
-		this.parent.Boat.turn = turn;
-		this.parent.Boat.throttle = 1;
-		this.addEventListener("OnEnterFrame", function (dt) {
+    this.parent.Boat.turn = turn;
+    this.parent.Boat.throttle = 1;
+    this.addEventListener("OnEnterFrame", function (dt) {
 
-		});
-	});
+    });
+  });
 
 }(this, this.TANK = this.TANK ||
 {}));
